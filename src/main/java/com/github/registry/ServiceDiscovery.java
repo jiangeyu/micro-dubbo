19bb5fcae753907baece25d22607a5a556ac4172
pjx <lingxiao@2dfire.com>
2018-06-27 10:37:27 +0800

    添加readme

--- src/main/java/com/github/registry/ServiceDiscovery.java
+++ src/main/java/com/github/registry/ServiceDiscovery.java
@@ -3,7 +3,7 @@ package com.github.registry;
 /**
  * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
  * @time: Created in 上午10:12 2018/6/27
- * @desc
+ * @desc 服务发现
  */
 public class ServiceDiscovery {
 }
--- src/main/java/com/github/registry/ServiceRegistry.java
+++ src/main/java/com/github/registry/ServiceRegistry.java
@@ -3,7 +3,7 @@ package com.github.registry;
 /**
  * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
  * @time: Created in 上午10:13 2018/6/27
- * @desc
+ * @desc zookeeper服务注册
  */
 public class ServiceRegistry {
 }
