da978b715101197f36787aa80899dd7d62fd98ce
pjx <lingxiao@2dfire.com>
2018-06-28 23:16:56 +0800

    注册地址constants

---
+++ src/main/java/com/github/constant/RegistryContants.java
@@ -0,0 +1,15 @@
+package com.github.constant;
+
+/**
+ * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
+ * @time: Created in 下午11:08 2018/6/28
+ * @desc
+ */
+public class RegistryContants {
+
+    public static int ZK_SESSION_TIMEOUT = 5000;
+
+    public static String ZK_REGISTRY_PATH = "/registry";
+
+    public static String ZK_DATA_PATH = ZK_REGISTRY_PATH + "/data";
+}
