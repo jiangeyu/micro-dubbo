ec5f8acf74df68de72a3e7d6263688345dbea75a
pjx <pjx2015@qq.com>
2018-06-29 18:10:50 +0800

    服务注册单元测试通过

---
+++ src/test/java/ZookeeperTest.java
@@ -0,0 +1,16 @@
+import com.github.registry.ServiceRegistry;
+import org.junit.Test;
+
+/**
+ * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
+ * @time: Created in 下午5:59 2018/6/29
+ * @desc
+ */
+public class ZookeeperTest {
+
+    @Test
+    public void zookeeperRegistryTest() {
+        ServiceRegistry registry = new ServiceRegistry("127.0.0.1:2181");
+        registry.registry("myZookeeper");
+    }
+}
