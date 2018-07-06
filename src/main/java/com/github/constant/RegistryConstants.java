25952a5e0e70dbe8f64a68d6c3bef782d9fca0cb
pjx <pjx2015@qq.com>
2018-07-06 22:12:20 +0800

    const

--- src/main/java/com/github/constant/RegistryConstants.java
+++ src/main/java/com/github/constant/RegistryConstants.java
@@ -3,13 +3,13 @@ package com.github.constant;
 /**
  * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
  * @time: Created in 下午11:08 2018/6/28
- * @desc
+ * @desc 常量类
  */
 public interface RegistryConstants {
 
-    public static int ZK_SESSION_TIMEOUT = 50000;
+    int ZK_SESSION_TIMEOUT = 50000;
 
-    public static String ZK_REGISTRY_PATH = "/registry";
+    String ZK_REGISTRY_PATH = "/registry";
 
-    public static String ZK_DATA_PATH = ZK_REGISTRY_PATH + "/data";
+    String ZK_DATA_PATH = ZK_REGISTRY_PATH + "/data";
 }
